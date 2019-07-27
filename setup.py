c65008ce74ab8716943a2029a52af592d160a978
Joshua Ashkinaze <52294640+josh-ashkinaze@users.noreply.github.com>
2019-07-27 12:07:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from distutils.core import setup
 
+
+
 setup(
   include_package_data = True,
   name = 'emotapal',
   packages = ['emotapal'],   
-  version = '0.7',      
+  version = '0.8',      
   license='gpl-3.0',        
   description = 'Match colors and words',   
   author = 'Josh Ashkinaze',                   
   author_email = 'josh.ashkinaze@gmail.com',   
   url = 'https://github.com/josh-ashkinaze/emotapal',  
-  download_url = 'https://github.com/josh-ashkinaze/emotapal/archive/v_07.tar.gz',    
-  keywords = ['colors', 'emotions', 'psychology', 'image processing'],    
+  download_url = 'https://github.com/josh-ashkinaze/emotapal/archive/v_08.tar.gz',    
+  keywords = ['colors', 'emotions', 'psychology', 'image processing'],  
   install_requires=[           
       "requests", "scikit-learn", "seaborn",
       "matplotlib", "colorthief", "google_images_download", "afinn"
@@ -20,10 +22,9 @@ setup(
   package_data={'emotapal': ['clf.pkl']},
 
   classifiers=[
-    'Development Status :: 3 - Alpha',       
+    'Development Status :: 3 - Alpha',     
     'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      #
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
